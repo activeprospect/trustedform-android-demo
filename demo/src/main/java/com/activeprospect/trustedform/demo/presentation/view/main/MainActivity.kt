e377b100573630d81e9ca74e00557325feb9b57e
devscale-rlu <79461692+devscale-rlu@users.noreply.github.com>
2021-04-19 16:42:40 +0200

    0.2.1alpha (#8)
    Fix for 'create certificate' error (SDK 0.2.7)

--- demo/src/main/java/com/activeprospect/trustedform/demo/presentation/view/main/MainActivity.kt
+++ demo/src/main/java/com/activeprospect/trustedform/demo/presentation/view/main/MainActivity.kt
@@ -1,12 +1,10 @@
 package com.activeprospect.trustedform.demo.presentation.view.main
 
 import android.os.Bundle
-import android.view.MotionEvent
 import com.activeprospect.trustedform.R
 import com.activeprospect.trustedform.databinding.ActivityMainBinding
 import com.activeprospect.trustedform.demo.common.commonview.BaseActivity
 import com.activeprospect.trustedform.demo.di.main.MainInjector
-import com.activeprospect.trustedform.sdk.api.TrustedForm
 import javax.inject.Inject
 
 class MainActivity(override val layoutId: Int = R.layout.activity_main) :
@@ -21,11 +19,6 @@ class MainActivity(override val layoutId: Int = R.layout.activity_main) :
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         injector.inject(this)
-    }
-
-    override fun dispatchTouchEvent(ev: MotionEvent): Boolean {
-        TrustedForm.default.onTouchEvent(ev)
-        return super.dispatchTouchEvent(ev)
     }
 
     override fun onBackPressed() {
